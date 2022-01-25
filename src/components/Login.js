55030ec131d68bb005f255e08172333a06d7f5eb
Hung <nso99tn@gmail.com>
2022-01-25 23:43:17 +0700

    update

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Login(props) {
-  return <div>Day la trang login</div>;
+  return <div>Day la trang login da chinh sua roi dó</div>;
 }
 
 export default Login;
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Post(props) {
-  return <div>Day la trang post</div>;
+  return <div>Day la trang post da chinh sua roi do</div>;
 }
 
 export default Post;
